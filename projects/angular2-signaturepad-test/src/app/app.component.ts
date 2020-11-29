c9e614dc839d4a3fa3b244c8572bbb14afd3ef4c
Zain Ahmad Khan <niazi_zain@hotmail.com>
2020-11-29 22:12:48 +0500

    Testing project

---
+++ projects/angular2-signaturepad-test/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, ViewChild } from '@angular/core';
+import { SignaturePad } from '../../../angular2-signaturepad/src/public-api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  @ViewChild(SignaturePad) signaturePad: SignaturePad;
+
+  signaturePadOptions = {
+    // passed through to szimek/signature_pad constructor
+    minWidth: 5,
+    canvasWidth: 500,
+    canvasHeight: 300,
+  };
+
+  constructor() {
+    // no-op
+  }
+
+  ngAfterViewInit() {
+    // this.signaturePad is now available
+    this.signaturePad.set('minWidth', 5); // set szimek/signature_pad options at runtime
+    this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
+  }
+
+  drawComplete() {
+    // will be notified of szimek/signature_pad's onEnd event
+    console.log(this.signaturePad.toDataURL());
+  }
+
+  drawStart() {
+    // will be notified of szimek/signature_pad's onBegin event
+    console.log('begin drawing');
+  }
+}
